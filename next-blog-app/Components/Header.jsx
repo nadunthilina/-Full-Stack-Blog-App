dc22d409b63eda8b1a6baddc22097069840b37e7
Nadun Thilina <152951157+nadunthilina@users.noreply.github.com>
2025-03-31 18:54:21 +0530

    add latest blog & <p> tag

--- next-blog-app/Components/Header.jsx
+++ next-blog-app/Components/Header.jsx
@@ -23,6 +23,16 @@ const Header = () => {
           />
         </button>
       </div>
+      <div className='text-center my-8'>
+        <h1 className='text-3xl sm:text-5xl font-medium'>Latest Blogs</h1>
+        <p className='mt-10 max-w-[700px] m-auto text-xs sm:text-base'>Stay updated with our latest blogs featuring insights, trends, and tips.Discover the latest updates, reviews, and thought-provoking discussions.</p>
+        <form className='flex justify-between max-w-[500px] scale-75 sm:scale-100 mx-auto mt-10 border border-black' action="">
+          <input type="email" placeholder='Enter Your Email' className='pl-4 outline-none'/>
+          <button className='border-1 border-black py-4 px-4 sm:px-8 active:bg-gray-600  active:text-white'>Subscribe</button>
+        </form>
+
+
+      </div>
     </div>
   );
 }
