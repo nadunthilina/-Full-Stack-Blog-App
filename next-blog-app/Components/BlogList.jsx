3ef088ad70856505ddbfacd411f005f271798224
Nadun Thilina <152951157+nadunthilina@users.noreply.github.com>
2025-04-01 16:45:55 +0530

    adding all blogs

--- next-blog-app/Components/BlogList.jsx
+++ next-blog-app/Components/BlogList.jsx
@@ -1,4 +1,6 @@
+import { blog_data } from '@/Assets/assets'
 import React from 'react'
+import BlogItem from './BlogItem'
 
 const BlogList = () => {
   return (
@@ -8,6 +10,13 @@ const BlogList = () => {
             <button>Tecnology</button>
             <button>Startup</button>
             <button>Lifestyle</button>
+
+        </div>
+        <div className='flex flex-wrap justify-around gap-1 gap-y-10 mb-16 xl:mx-24'>
+            {blog_data.map((items,index)=>{
+                return <BlogItem key={index} image={items.image} title={items.title} description={items.description} category={items.category}/>
+
+            })}
 
         </div>
     </div>
