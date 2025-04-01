4e5c889bc748c4444eba3929171d875ae175690b
Nadun Thilina <152951157+nadunthilina@users.noreply.github.com>
2025-04-01 16:34:33 +0530

    create BlogList.jsx

---
+++ next-blog-app/Components/BlogList.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const BlogList = () => {
+  return (
+    <div>
+        <div className='flex justify-center gap-6 my-10'>
+            <button className='bg-black text-white py-1 px4 rounded-sm'>All</button>
+            <button>Tecnology</button>
+            <button>Startup</button>
+            <button>Lifestyle</button>
+
+        </div>
+    </div>
+  )
+}
+
+export default BlogList
