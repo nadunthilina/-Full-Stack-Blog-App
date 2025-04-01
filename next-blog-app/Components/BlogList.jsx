2aa9cd4f34bf3e37b3ba4d55bf34485c63323eb3
Nadun Thilina <152951157+nadunthilina@users.noreply.github.com>
2025-04-01 17:24:40 +0530

    create separate blogs

--- next-blog-app/Components/BlogList.jsx
+++ next-blog-app/Components/BlogList.jsx
@@ -1,26 +1,29 @@
+"use client";
+
 import { blog_data } from '@/Assets/assets'
-import React from 'react'
+import React, { useState } from 'react'
 import BlogItem from './BlogItem'
 
 const BlogList = () => {
-  return (
-    <div>
-        <div className='flex justify-center gap-6 my-10'>
-            <button className='bg-black text-white py-1 px4 rounded-sm'>All</button>
-            <button>Tecnology</button>
-            <button>Startup</button>
-            <button>Lifestyle</button>
+    const [menu, setMenu] = useState("All");
 
+    return (
+        <div>
+            <div className='flex justify-center gap-6 my-10'>
+                <button onClick={() => setMenu('All')} className={`${menu === "All" ? "bg-black text-white" : "bg-gray-200"} py-1 px-4 rounded-sm`}>All</button>
+                <button onClick={() => setMenu('Technology')} className={`${menu === "Technology" ? "bg-black text-white" : "bg-gray-200"} py-1 px-4 rounded-sm`}>Technology</button>
+                <button onClick={() => setMenu('Startup')} className={`${menu === "Startup" ? "bg-black text-white" : "bg-gray-200"} py-1 px-4 rounded-sm`}>Startup</button>
+                <button onClick={() => setMenu('Lifestyle')} className={`${menu === "Lifestyle" ? "bg-black text-white" : "bg-gray-200"} py-1 px-4 rounded-sm`}>Lifestyle</button>
+            </div>
+            <div className='flex flex-wrap justify-around gap-1 gap-y-10 mb-16 xl:mx-24'>
+                {blog_data
+                  .filter(item => menu === "All" || item.category === menu)
+                  .map((item, index) => (
+                      <BlogItem key={index} image={item.image} title={item.title} description={item.description} category={item.category} />
+                  ))}
+            </div>
         </div>
-        <div className='flex flex-wrap justify-around gap-1 gap-y-10 mb-16 xl:mx-24'>
-            {blog_data.map((items,index)=>{
-                return <BlogItem key={index} image={items.image} title={items.title} description={items.description} category={items.category}/>
-
-            })}
-
-        </div>
-    </div>
-  )
+    )
 }
 
-export default BlogList
+export default BlogList;
