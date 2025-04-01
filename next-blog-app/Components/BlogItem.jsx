907b50401081bdeb5618ac04fc814c970f5b4e61
Nadun Thilina <152951157+nadunthilina@users.noreply.github.com>
2025-04-01 15:47:49 +0530

    add blog description

--- next-blog-app/Components/BlogItem.jsx
+++ next-blog-app/Components/BlogItem.jsx
@@ -8,7 +8,10 @@ export default function BlogItem() {
     <div className="max-w-[330px] sm:max-w-[300px] bg-white border border-black">
       <Image src={blog_data[0].image} alt="" width={400} height={400} className="border-b border-black" />
       <p className="ml-5 mt-5vpx-1 inline-block bg-black text-white text-sm">{blog_data[0].category}</p>
-      
+      <div className="p-5">
+        <h5 className="mb-2 text-lg font-medium tracking-tight text-gray-900">{blog_data[0].title}</h5>
+        <p className="mb-3 text-sm tracking-tight text-gray-700">{blog_data[0].description}</p>
+      </div>
     </div>
   );
 }
