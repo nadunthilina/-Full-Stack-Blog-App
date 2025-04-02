ef97943f8ece6556108f3a551b8e7e665ee1e63d
Nadun Thilina <152951157+nadunthilina@users.noreply.github.com>
2025-04-02 18:13:58 +0530

    add social  media icons

--- next-blog-app/Components/Footer.jsx
+++ next-blog-app/Components/Footer.jsx
@@ -6,6 +6,12 @@ const Footer = () => {
   return (
     <div className='flex justify-around flex-col gap-2 sm:gap-0 sm:flex-row bg-white py-5 items-center'>
       <Image src={assets.nlogo} alt='Logo' width={180} height={150} />  {/* ✅ Added height */}
+      <p className='text-sm text-black'>All Right Reserved . CopyRight @NadunCode 2025</p>
+      <div className='flex'>
+        <Image src={assets.facebook_icon} alt='' width={40}/>
+        <Image src={assets.twitter_icon} alt='' width={40}/>
+        <Image src={assets.googleplus_icon} alt='' width={40}/>
+      </div>
     </div>
   );
 }
