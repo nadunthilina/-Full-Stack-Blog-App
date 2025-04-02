9bca56ca198e9efbdfd151b7248d4559f2ba91fa
Nadun Thilina <152951157+nadunthilina@users.noreply.github.com>
2025-04-02 21:27:37 +0530

    update page.jsx

--- next-blog-app/app/blogs/[id]/page.jsx
+++ next-blog-app/app/blogs/[id]/page.jsx
@@ -1,31 +1,34 @@
-'use client'
-import { blog_data } from '@/Assets/assets';
-import React, { useEffect, useState } from 'react'
+"use client";
+import { assets, blog_data } from "@/Assets/assets";
+import Image from "next/image";
+import React, { useEffect, useState } from "react";
 
-const page = ({params}) => {
+const BlogPage = ({ params }) => {
+  const [data, setData] = useState(null);
 
-    const[data,setData]= useState(null);
-
-    const fetchBlogData = () =>{
-        for(let i=0;i<blog_data.length;i++)
-        {
-            if(Number(params.id)===blog_data[i].id){
-                setData(blog_data[i]);
-
-            }
-        }
-
+  useEffect(() => {
+    if (params?.id) {
+      const blog = blog_data.find((item) => Number(params.id) === item.id);
+      setData(blog || null);
     }
+  }, [params.id]);
 
-    useEffect(()=>{
-        fetchBlogData();
+  if (!data) {
+    return <div className="text-center text-gray-500">Blog not found</div>;
+  }
 
-    },[])
   return (
-    <div>
-        {params.id}
-    </div>
-  )
-}
+    <div className='bg-gray-299 py-5 px-5 md:px-12 lg:px-28'>
+      <div className="flex justify-between items-center">
 
-export default page
+      </div>
+     
+      <Image src={assets.nlogo} width={180} alt="" className="w-[130px] sm:w-auto" />
+      <button className="flex items-center gap-2 font-medium py-1 px-3 sm:py-3 sm:px-6 border border-black shadow-[-7px_7px_0px_#000000]">
+        Get Started <Image src={assets.arrow} alt="" />
+      </button>
+    </div>
+  );
+};
+
+export default BlogPage;
