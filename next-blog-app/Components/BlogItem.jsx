4f274c7220a7452ca3fc7a847810df9628563e4e
Nadun Thilina <152951157+nadunthilina@users.noreply.github.com>
2025-04-02 18:53:10 +0530

    add blog id

--- next-blog-app/Components/BlogItem.jsx
+++ next-blog-app/Components/BlogItem.jsx
@@ -1,19 +1,28 @@
 "use client";
-import { assets, blog_data } from "@/Assets/assets";
+import { assets } from "@/Assets/assets";
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
-export default function BlogItem({title,description,category,image}) {
+export default function BlogItem({ id, title, description, category, image }) {
   return (
     <div className="max-w-[330px] sm:max-w-[300px] bg-white border border-black">
-      <Image src={image} alt="" width={400} height={400} className="border-b border-black" />
-      <p className="ml-5 mt-5vpx-1 inline-block bg-black text-white text-sm">{category}</p>
+      <Link href={`/blogs/${id}`}>
+        <Image src={image} alt={title} width={400} height={400} className="border-b border-black" />
+      </Link>
+
+      <p className="ml-5 mt-5 px-1 inline-block bg-black text-white text-sm">
+        {category} #{id}  {/* Blog Number */}
+      </p>
+
       <div className="p-5">
         <h5 className="mb-2 text-lg font-medium tracking-tight text-gray-900">{title}</h5>
         <p className="mb-3 text-sm tracking-tight text-gray-700">{description}</p>
-        <div className="inline-flex items-center py-2 font-semibold text-center">
-          Read More <Image  src={assets.arrow} className="ml-2" alt='' width={12}/>
-        </div>
+        
+        <Link href={`/blogs/${id}`} className="inline-flex items-center py-2 font-semibold text-center">
+          Read More 
+          <Image src={assets.arrow} className="ml-2" alt="arrow" width={12} height={12} />
+        </Link>
       </div>
     </div>
   );
